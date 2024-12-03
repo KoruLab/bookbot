dd1c1043c204ed229c90d5e1e49d12245f8cb51c
KoruLab <hcorujo@gmail.com>
2024-12-03 23:26:37 +0100

    C: contador de letras y símbolos

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from collections import Counter
 
-def main():
-    with open ("books/frankenstein.txt") as f:
+def main(s):
+    with open (s) as f:
         
         file_contents = f.read().split()
         contador = len(file_contents)
-    print(contador)
+    print(f"El número de palabras es {contador}")
+    print(cuenta_letras(s))
+
+def cuenta_letras(st):
+    with open (st) as f:
+        file_contents = f.read().lower()
+        contador = dict(Counter(file_contents).most_common())
+        #return(f"la frecuencia de letras es {letras}")
+        return(f"La frecuencia de letras y símbolos es {contador}")
 
 
+main("books/frankenstein.txt")
     
 
-main()
