1a5ede3ee97331ff12620cf0337f1f2f312187b7
KoruLab <hcorujo@gmail.com>
2024-12-03 23:30:51 +0100

    C: Cambio de sintaxis

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 from collections import Counter
 
 def main(s):
-    with open (s) as f:
+    with open (f"books/{s}.txt") as f:
         
         file_contents = f.read().split()
         contador = len(file_contents)
     print(f"El número de palabras es {contador}")
     print(cuenta_letras(s))
 
-def cuenta_letras(st):
-    with open (st) as f:
+def cuenta_letras(s):
+    with open (f"books/{s}.txt") as f:
         file_contents = f.read().lower()
         contador = dict(Counter(file_contents).most_common())
         #return(f"la frecuencia de letras es {letras}")
         return(f"La frecuencia de letras y símbolos es {contador}")
 
 
-main("books/frankenstein.txt")
+main("frankenstein")
     
 
